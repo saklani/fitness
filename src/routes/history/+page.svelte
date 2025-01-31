7e713c43778b48ff7afe7b8fec3ac94f99534319
saklani <50768838+saklani@users.noreply.github.com>
2025-01-30 21:08:39 -0500

    consistency

--- src/routes/history/+page.svelte
+++ src/routes/history/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+    import Header from "@app/components/Header.svelte";
     import History from "@app/components/History.svelte";
     import * as Card from "@app/ui/card";
 
     const { data } = $props();
 </script>
 
-<h1 class="title pb-[24px]">Workout History</h1>
+<Header>
+    <h1 class="title text-lg">Workout History</h1>
+</Header>
 {#each data.workouts as workout}
     <History {workout} />
 {:else}
